e5011e7122ad40cd7381a334b7ac388ea4f46874
Catrin Hughes <catrinhughes92@gmail.com>
2020-05-08 14:23:56 +0100

    nav links scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 /*eslint-env jquery*/
 
 $('document').ready(function () {
-    
+
     /* STICKY NAV */
     $('.js--section-features').waypoint(function (direction) {
         if (direction === "down") {
@@ -12,14 +12,46 @@ $('document').ready(function () {
     }, {
         offset: '60px;'
     });
-    
+
     /* SCROLL ON BUTTONS */
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
-    
+
     $('.js--scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
+
+    /* NAVIGATION SCROLL */
+
+        $('a[href*="#"]')
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function(event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+                && 
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); 
+                            $target.focus();
+                        }
+                    });
+                }
+            }
+        });
+
 });
 

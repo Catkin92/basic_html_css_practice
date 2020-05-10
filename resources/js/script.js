d4a65e085c2a181566eb798be4bc803fbc919919
Catrin Hughes <catrinhughes92@gmail.com>
2020-05-10 15:06:08 +0100

    mobile nav with jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,6 +12,24 @@ $('document').ready(function () {
     }, {
         offset: '60px;'
     });
+    
+    /* MOBILE NAV */
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+        
+    });
 
     /* SCROLL ON BUTTONS */
     $('.js--scroll-to-plans').click(function () {
@@ -78,5 +96,6 @@ $('document').ready(function () {
     }, {
         offset: '50%;'
     })
+    
 });
 
